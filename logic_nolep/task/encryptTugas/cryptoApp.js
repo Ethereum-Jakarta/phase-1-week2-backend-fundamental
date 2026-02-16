ecc168c99a036e7a82983226859dc33cced3eb28
Radit <primaalfiansyah@gmail.com>
2026-02-16 16:27:00 +0700

    feat: submit encryptTugas

---
+++ logic_nolep/task/encryptTugas/cryptoApp.js
@@ -0,0 +1,18 @@
+const CryptoJS = require('crypto-js');
+
+function encrypt(text, key) {
+  //code
+  let encryptedText = CryptoJS.AES.encrypt(text, key).toString();
+
+  return encryptedText;
+}
+
+function decrypt(encryptedText, key) {
+   //code
+   let bytes  = CryptoJS.AES.decrypt(encryptedText, key);
+   let originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+   return originalText;
+}
+
+module.exports = { encrypt, decrypt };
